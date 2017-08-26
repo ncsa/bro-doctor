2cec0fe1ab2a4c940b4e3c72e42c71dac599dee8
Justin Azoff <justin@bouncybouncy.net>
2017-08-26 17:23:29 -0400

    capture loss stats

--- doctor.py
+++ doctor.py
@@ -1,23 +1,80 @@
 import BroControl.plugin
 from BroControl import config
 
-import os
+from collections import defaultdict, namedtuple
+import gzip
 import glob
+import json
+import os
+
+def bro_ascii_reader(f):
+    line = ''
+    headers = {}
+    it = iter(f)
+    while not line.startswith("#types"):
+        line = next(it).rstrip()
+        k,v = line[1:].split(None, 1)
+        headers[k] = v
+
+    sep = headers['separator'].decode("string-escape")
+
+    for k,v in headers.items():
+        if sep in v:
+            headers[k] = v.split(sep)
+
+    headers['separator'] = sep
+    fields = headers['fields']
+    types = headers['types']
+    set_sep = headers['set_separator']
+
+    vectors = [field for field, type in zip(fields, types) if type.startswith("vector[")]
+
+    for row in it:
+        if row.startswith("#close"): break
+        parts = row.rstrip().split(sep)
+        rec = dict(zip(fields, parts))
+        for f in vectors:
+            rec[f] = rec[f].split(set_sep)
+        yield rec
+
+def bro_json_reader(f):
+    for line in f:
+        yield json.loads(f)
+
+def open_log(filename):
+    if filename.endswith(".log"):
+        return open(filename)
+    if filename.endswith(".gz"):
+        return gzip.open(filename)
+    raise Exception("Unknown log extension: {}".format(filename))
+
+def read_bro_log(filename):
+    reader = None
+    with open_log(filename) as f:
+        first_byte = f.read(1)
+        if first_byte == '#':
+            reader = bro_ascii_reader
+        if first_byte == '{':
+            reader = bro_json_reader
+    if not reader:
+        raise Exception("Unknown bro log type, first line: {!r}".format(f.readline().strip()))
+
+    f = open_log(filename)
+    for rec in reader(f):
+        yield rec
+    f.close()
 
 def find_recent_log_directories(base_dir, days=7):
     dirs = os.listdir(base_dir)
     dirs.sort()
     #Fix in 983 years
-    return [d for d in dirs if d.startswith("20")][:days]
+    return [os.path.join(base_dir, d) for d in dirs if d.startswith("20")][-days:]
 
 def find_recent_log_files(base_dir, glob_pattern,  days=7):
-    dirs = os.listdir(base_dir)
-    dirs.sort()
-    #Fix in 983 years
-    dirs = [d for d in dirs if d.startswith("20")][:days]
+    dirs = find_recent_log_directories(base_dir, days)
     matches = []
     for d in dirs:
-        matches.extend(glob.glob(os.path.join(d, glob_pattern)))
+        matches.extend(glob.glob(os.path.join(base_dir, d, glob_pattern)))
     return matches
         
 
@@ -56,12 +113,33 @@ class Doctor(BroControl.plugin.Plugin):
             self.error("No capture_loss logs in the past {} days".format(GOBACK) )
             return False
 
+        workers = defaultdict(list)
+        want_records = 10000
+        for f in reversed(files):
+            for rec in read_bro_log(f):
+                want_records -= 1
+                if not want_records:
+                    break
+                workers[rec['peer']].append(rec)
+
+        print "Capture loss stats:"
+        for w, recs in sorted(workers.items()):
+            min_loss = min(float(rec['percent_lost']) for rec in recs)
+            max_loss = max(float(rec['percent_lost']) for rec in recs)
+            total_gaps = sum(int(rec['gaps']) for rec in recs)
+            total_acks = sum(int(rec['acks']) for rec in recs)
+            overall_pct = 100.0 * total_gaps/total_acks
+            loss_count = sum(1 for rec in recs if float(rec['percent_lost']) != 0.0)
+            noloss_count = len(recs) - loss_count
+
+            print "worker={} loss_count={} noloss_count={} min_loss={} max_loss={} overall_loss={}".format(w, loss_count, noloss_count, min_loss, max_loss, overall_pct)
+        
     def cmd_custom(self, cmd, args, cmdout):
         self.message("Using log directory {}".format(self.log_directory))
         results = BroControl.cmdresult.CmdResult()
         results.ok = True
 
-        for f in [self.check_reporter]:
+        for f in [self.check_reporter, self.check_capture_loss]:
             results.ok = f() and results.ok
 
         return results
