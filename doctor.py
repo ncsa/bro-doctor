480b0445912a9850fa87be87bf8102b0da39a7c3
Justin Azoff <justin@bouncybouncy.net>
2017-08-31 18:34:33 -0400

    many improvements.

--- doctor.py
+++ doctor.py
@@ -8,6 +8,18 @@ import json
 import os
 import subprocess
 
+        
+GOBACK = 7 # days
+LOSS_THRESHOLD = 1
+
+RED = '\033[91m'
+ENDC = '\033[0m'
+GREEN = '\033[92m'
+def red(s):
+    return RED + s + ENDC
+def green(s):
+    return GREEN + s + ENDC
+
 def bro_ascii_reader(f):
     line = ''
     headers = {}
@@ -65,6 +77,16 @@ def read_bro_log(filename):
         yield rec
     f.close()
 
+def read_bro_logs_with_line_limit(filenames, limit=10000):
+    # TODO: just use itertools.islice?
+    for f in filenames:
+        for rec in read_bro_log(f):
+            yield rec
+            limit -= 1
+            if limit == 0:
+                return
+        
+
 def find_recent_log_directories(base_dir, days=7):
     dirs = os.listdir(base_dir)
     dirs.sort()
@@ -77,9 +99,6 @@ def find_recent_log_files(base_dir, glob_pattern,  days=7):
     for d in dirs:
         matches.extend(glob.glob(os.path.join(base_dir, d, glob_pattern)))
     return matches
-        
-
-GOBACK = 7 # days
 
 class Doctor(BroControl.plugin.Plugin):
     def __init__(self):
@@ -92,8 +111,6 @@ class Doctor(BroControl.plugin.Plugin):
         return 1
 
     def init(self):
-        self.message("Doctor plugin is initialized")
-
         self.log_directory = self.getGlobalOption("logdir")
         self.bro_binary = self.getGlobalOption("bro")
         return True
@@ -101,28 +118,53 @@ class Doctor(BroControl.plugin.Plugin):
     def commands(self):
         return [("bro", "", "Troubleshoot Bro installation")]
 
+    def err(self, msg):
+        self.error(red(msg))
+
+    def ok(self, msg):
+        self.message(green(msg))
+    def ok_if(self, msg, is_ok):
+        if is_ok:
+            self.ok(msg)
+        else:
+            self.err(msg)
+
     def check_reporter(self):
-        """"Check for recent reporter.log entries"""
+        """Checking for recent reporter.log entries"""
         files = find_recent_log_files(self.log_directory, "reporter.*", days=GOBACK)
         if not files:
-            self.message("No reporter logs in the past {} days".format(GOBACK) )
+            self.message("No reporter log files in the past {} days".format(GOBACK) )
             return True
+        self.err("Found {} reporter log files in the past {} days".format(len(files), GOBACK) )
+
+        self.err("100 most recent reporter.log errors:")
+        seen = set()
+        suppressed = 0
+        for rec in read_bro_logs_with_line_limit(reversed(files), 100):
+            if rec['ts'] == '0.000000':
+                rec['ts'] = ''
+            if rec['location'] == '(empty)':
+                rec['location'] = ''
+            m = "{location} {ts} {level} {message}".format(**rec).lstrip()
+            if m not in seen:
+                self.message(red(m))
+                seen.add(m)
+            else:
+                suppressed += 1
+        if suppressed:
+            self.err("suppressed {} duplicate messages".format(suppressed))
 
     def check_capture_loss(self):
-        """"Check for recent capture_loss.log entries"""
+        """Checking for recent capture_loss.log entries"""
         files = find_recent_log_files(self.log_directory, "capture_loss.*", days=GOBACK)
         if not files:
-            self.error("No capture_loss logs in the past {} days".format(GOBACK) )
+            self.err("No capture_loss log files in the past {} days".format(GOBACK) )
+            self.err("Add '@load misc/capture-loss' to your local.bro")
             return False
 
         workers = defaultdict(list)
-        want_records = 10000
-        for f in reversed(files):
-            for rec in read_bro_log(f):
-                want_records -= 1
-                if not want_records:
-                    break
-                workers[rec['peer']].append(rec)
+        for rec in read_bro_logs_with_line_limit(reversed(files), 10000):
+            workers[rec['peer']].append(rec)
 
         self.message("Capture loss stats:")
         for w, recs in sorted(workers.items()):
@@ -134,28 +176,31 @@ class Doctor(BroControl.plugin.Plugin):
             loss_count = sum(1 for rec in recs if float(rec['percent_lost']) != 0.0)
             noloss_count = len(recs) - loss_count
 
-            self.message("worker={} loss_count={} noloss_count={} min_loss={} max_loss={} overall_loss={}".format(w, loss_count, noloss_count, min_loss, max_loss, overall_pct))
+            msg = "worker={} loss_count={} noloss_count={} min_loss={} max_loss={} overall_loss={}".format(w, loss_count, noloss_count, min_loss, max_loss, overall_pct)
+            self.ok_if(msg, overall_pct >= LOSS_THRESHOLD)
 
     def check_pfring(self):
-        """Check to see if bro is linked against pf_ring if it is configured to use it"""
+        """Checking if bro is linked against pf_ring if lb_method is pf_ring"""
 
         pfring_configured = any(n.lb_method == 'pf_ring' for n in self.nodes())
 
         bro_ldd = subprocess.check_output(["ldd", self.bro_binary])
         pfring_linked = 'pfring' in bro_ldd
 
-        if pfring_configured == pfring_linked:
-            self.message("pf_ring: ok! configured={} linked={}".format(pfring_configured, pfring_linked))
-        else:
-            self.error("pf_ring: error! configured={} linked={}".format(pfring_configured, pfring_linked))
+        msg = "configured to use pf_ring={}. linked against pf_ring={}".format(pfring_configured, pfring_linked)
+        self.ok_if(msg, pfring_configured == pfring_linked)
         
     def cmd_custom(self, cmd, args, cmdout):
         self.message("Using log directory {}".format(self.log_directory))
         results = BroControl.cmdresult.CmdResult()
         results.ok = True
 
+        self.message("Running checks...")
         for f in [self.check_pfring, self.check_reporter, self.check_capture_loss]:
+            self.message(f.__doc__)
+            self.message("#" * len(f.__doc__))
             results.ok = f() and results.ok
+            self.message('')
 
         return results
 
